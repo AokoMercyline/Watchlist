a876105552c6916d798bf19e031b011390e9a4e3
AokoMercyline <aokomercyline34@gmail.com>
2020-09-11 17:06:06 +0300

    deployment to heroku

--- app/config.py
+++ app/config.py
@@ -1,34 +1,20 @@
 import os
+
 class Config:
-    '''
-    General configuration parent class
-    '''
+
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('+D\xdf\x11\xe7\x06Fq?\x85\xbc\xc3')
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
-
-
 class ProdConfig(Config):
-    '''
-    Pruduction  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
     DEBUG = True
-    
+
 config_options = {
-    'development':DevConfig,
-    'production':ProdConfig
+'development':DevConfig,
+'production':ProdConfig
 }
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY='47ce875a03901c7f9fa0bba738be18a3'
+export SECRET_KEY='+D\xdf\x11\xe7\x06Fq?\x85\xbc\xc3'
+
+python3.8 manage.py server
