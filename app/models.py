e5b6f43bba60b62204149cf9c137db8611558c2a
AokoMercyline <aokomercyline34@gmail.com>
2020-09-16 14:48:50 +0300

    imported instance and user class and create a shell context decorator to create a shell context

--- app/models.py
+++ app/models.py
@@ -46,9 +46,16 @@ class Review:
         return response
     
  class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
+    __tablename__ = 'users' # allows us to give the tables in our database proper names
+    id = db.Column(db.Integer,primary_key = True) # creates colomn
     username = db.Column(db.String(255))
 
     def __repr__(self):
-        return f'User {self.username}'   
+        return f'User {self.username}' 
+    
+    @manager.shell
+    def make_shell_context():
+    return dict(app = app,db = db,User = User )
+
+    if __name__ == '__main__':
+    manager.run()
