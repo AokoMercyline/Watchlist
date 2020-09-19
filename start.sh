de2e44ecf7a9503bbdbdd6d845881e21dd5dad98
AokoMercyline <aokomercyline34@gmail.com>
2020-09-19 11:23:36 +0300

    created login and profile for user,linked up the user with the email

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 export MOVIE_API_KEY='47ce875a03901c7f9fa0bba738be18a3'
 export SECRET_KEY='+D\xdf\x11\xe7\x06Fq?\x85\xbc\xc3'
 
+export MAIL_USERNAME='aokomercyline34@gmail.com'
+export MAIL_PASSWORD='mao2019abc'
+python3.6 manage.py server
+
 python3.8 manage.py server
